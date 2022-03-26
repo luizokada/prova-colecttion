fd70fea99344c5e3fbba68c890adef769b4e6b4b
luizokada <luizfernandookada@gmail.com>
2022-03-26 00:48:52 -0300

    Arrumei a API no request PUT

--- api/controllers/produtoController.js
+++ api/controllers/produtoController.js
@@ -1,68 +1,78 @@
 import produtos from "../models/Produto.js";
 
 class produtoController {
+  static listarProdutos = (req, res) => {
+    produtos.find((err, produtos) => {
+      res.status(200).json(produtos);
+    });
+  };
 
-    static listarProdutos = (req, res) => {
-        produtos.find((err, produtos) => {
-            res.status(200).json(produtos);
+  static listarProdutoPorNome = (req, res) => {
+    const nome = req.query.nome;
+    produtos.find({ nome: nome }, {}, (err, produtos) => {
+      if (err) {
+        res.status(500).send({
+          message: `${err.message} -  Erro ao cadastrar o novo produto`,
         });
+      } else {
+        res.status(200).send(produtos);
+      }
+    });
+  };
+
+  static cadastrarProduto = (req, res) => {
+    let produto = new produtos(req.body);
+
+    produto.save((err) => {
+      if (err) {
+        res.status(500).send({
+          message: `${err.message} -  Erro ao cadastrar o novo produto`,
+        });
+      } else {
+        res.status(201).send(produto.toJSON());
+      }
+    });
+  };
+  static atualizarProduto = (req, res) => {
+    const id = req.params.id;
+
+    if (req.body.ativo) {
+      const newProduto = { ...req.body, dataInativacao: "" };
+      produtos.findByIdAndUpdate(id, { $set: newProduto }, (err) => {
+        if (!err) {
+          res.status(200).send({ message: `Produto alterado com sucesso` });
+        } else {
+          res
+            .status(500)
+            .send({ message: `${err.message} - Erro na atualização` });
+        }
+      });
+    } else {
+      const data = new Date();
+      const newProduto = { ...req.body, dataInativacao: data };
+      produtos.findByIdAndUpdate(id, { $set: newProduto }, (err) => {
+        if (!err) {
+          res.status(200).send({ message: `Produto alterado com sucesso` });
+        } else {
+          res
+            .status(500)
+            .send({ message: `${err.message} - Erro na atualização` });
+        }
+      });
     }
+  };
 
-    static listarProdutoPorNome = (req, res) => {
-        const nome = req.query.nome;
-        produtos.find({ 'nome': nome }, {}, (err, produtos) => {
+  static excluirProduto = (req, res) => {
+    const id = req.params.id;
 
-            if (err) {
-                res.status(500).send({ message: `${err.message} -  Erro ao cadastrar o novo produto` });
-            }
-            else {
-                res.status(200).send(produtos);
-            }
-        })
-    }
-
-    static cadastrarProduto = (req, res) => {
-        let produto = new produtos(req.body);
-
-        produto.save((err) => {
-
-            if (err) {
-                res.status(500).send({ message: `${err.message} -  Erro ao cadastrar o novo produto` });
-            }
-            else {
-                res.status(201).send(produto.toJSON());
-            }
-        })
-
-    }
-    static atualizarProduto = (req, res) => {
-        const id = req.params.id;
-
-        produtos.findByIdAndUpdate(id, { $set: req.body }, (err) => {
-            if (!err) {
-                res.status(200).send({ message: `Produto alterado com sucesso` })
-            }
-            else {
-                res.status(500).send({ message: `${err.message} - Erro na atualização` })
-            }
-
-        })
-
-    }
-
-    static excluirProduto = (req, res) => {
-        const id = req.params.id;
-
-        produtos.findByIdAndDelete(id, (err) => {
-            if (!err) {
-                res.status(200).send(`Produto deletado com sucesso`)
-            }
-            else {
-                res.status(500).send(`${err.message} - Erro na exclusao`)
-            }
-        })
-    }
-
+    produtos.findByIdAndDelete(id, (err) => {
+      if (!err) {
+        res.status(200).send(`Produto deletado com sucesso`);
+      } else {
+        res.status(500).send(`${err.message} - Erro na exclusao`);
+      }
+    });
+  };
 }
 
 export default produtoController;
--- api/models/Produto.js
+++ api/models/Produto.js
@@ -1,19 +1,15 @@
 import mongoose from "mongoose";
 
-const produtoSchema = new mongoose.Schema(
-    {
-        id: { type: String },
-        nome: { type: String, required: true },
-        marca: { type: String, required: true },
-        thumb: { type: String, required: true },
-        dataInativacao: { type: Date },
-        ativo: { type: Boolean, required: true },
-        descricao: { type: String, required: true }
-    }
-)
+const produtoSchema = new mongoose.Schema({
+  id: { type: String },
+  nome: { type: String, required: true },
+  marca: { type: String, required: true },
+  thumb: { type: String, required: true },
+  dataInativacao: { type: Date },
+  ativo: { type: Boolean, required: true },
+  descricao: { type: String, required: true },
+});
 
-const produtos = mongoose.model('produtos', produtoSchema);
-
-produtos.createIndexes({ nome: "text" })
+const produtos = mongoose.model("produtos", produtoSchema);
 
 export default produtos;
